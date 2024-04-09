5bc2fb75b6e11fb66c0da90faad9270d4b0d6c80
raissalages <105797320+raissalages@users.noreply.github.com>
2024-04-08 21:24:56 -0300

    Create TesteDoisSensoresUltrassonicos.ino

---
+++ Desenvolvimento/CasosTeste/TesteDoisSensoresUltrassonicos.ino
@@ -0,0 +1,68 @@
+const int trigPin1 = 8;
+const int echoPin1 = 7;
+const int trigPin2 = 13;
+const int echoPin2 = 12;
+
+long distancia;
+long distancia1;
+long distancia2;
+long duration;
+
+void setup()
+{
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(gatePin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+ 
+  // Sensor 1
+  distancia1 = atividadeSensor(trigPin1, echoPin1);
+  
+  // Sensor 2
+  distancia2 = atividadeSensor(trigPin2, echoPin2);
+
+  // Imprimir as distâncias
+  Serial.print("Sensor 1 - Distancia: ");
+  Serial.print(distancia1);
+  Serial.println(" cm");
+  
+  Serial.print("Sensor 2 - Distancia: ");
+  Serial.print(distancia2);
+  Serial.println(" cm");
+  
+  if(distancia1 < 30 || distancia2 < 30){
+    vibracao(100);
+  }else if (distancia1 < 100 || distancia2 < 100){
+    vibracao(300);
+
+  }else{
+    vibracao(400);
+
+  }
+  
+  delay(100);
+}
+
+long microssegundosParaCentimetros(long microssegundos){
+	return microssegundos / 29 / 2;
+}
+
+
+      
+long atividadeSensor(int trig, int echo){
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo, HIGH);
+  return distancia = microssegundosParaCentimetros(duration);
+}
